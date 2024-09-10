9cc491e562a09aaa9dffd9901d00bc3bcd819f6c
Matt Ao <mattyayoh@gmail.com>
2024-09-09 20:53:34 -0700

    uh weird circular reference issue went away...

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,12 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = 'User'  # Use the string name of the model here
+        model = User 
         fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
     
     def create(self, validated_data):
-        from django.contrib.auth.models import User  # Import here to avoid circular imports
         user = User.objects.create_user(**validated_data)
         return user
